6e1257693d586f045f402e88554a8f49e63b93c9
Chris <christiaangerritsen2000@gmail.com>
2019-04-16 17:34:14 +0200

    new commit

--- src/Form/Event1Type.php
+++ src/Form/Event1Type.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,7 +19,7 @@ class Event1Type extends AbstractType
             ->add('min_part')
             ->add('max_part')
             ->add('description', TextareaType::class, ['attr' => ['name' => 'editor'], ])
-            ->add('image')
+            ->add('image', FileType::class)
             ->add('name')
         ;
     }
--- src/Form/LocationType.php
+++ src/Form/LocationType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Location;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,7 @@ class LocationType extends AbstractType
         $builder
             ->add('location_name')
             ->add('seats')
-            ->add('image')
+            ->add('image', FileType::class)
         ;
     }
 
